4e95e17d7aa33fafa0d220fa79715134761e8fe8
shray0027 <59202921+shray0027@users.noreply.github.com>
2020-08-30 19:57:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+//img-1
+var randomNumber1= Math.floor(Math.random()*6)+1;
+var randomDiceImage1 = "dice"+randomNumber1+".png";
+var randomDiceImageSource1 ="images/"+randomDiceImage1;
+var image1 = document.querySelector(".img1").setAttribute("src", randomDiceImageSource1);
+
+
+//img-2
+var randomNumber2= Math.floor(Math.random()*6)+1;
+var randomDiceImage2 = "dice"+randomNumber2+".png";
+var randomDiceImageSource2 ="images/"+randomDiceImage2;
+var image1 = document.querySelector(".img2").setAttribute("src", randomDiceImageSource2);
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML="⛳️ Player 1 Wins";
+}
+else if(andomNumber1<randomNumber2){
+  document.querySelector("h1").innerHTML="Player 2 Wins ⛳️";
+}
+else{
+  document.querySelector("h1").innerHTML="Draw !";
+}
